b91db8aa73273ecf0215089218ea329e465a22de
lz11 <livspeng11@sina.com>
2015-12-31 10:22:31 +0800

    change 2

---
+++ JustTry/ThirdTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ThirdTableViewCell.h
+//  JustTry
+//
+//  Created by MS on 15/12/31.
+//  Copyright (c) 2015年 LP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdTableViewCell : UITableViewCell
+
+@end
---
+++ JustTry/ThirdTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ThirdTableViewCell.m
+//  JustTry
+//
+//  Created by MS on 15/12/31.
+//  Copyright (c) 2015年 LP. All rights reserved.
+//
+
+#import "ThirdTableViewCell.h"
+
+@implementation ThirdTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
