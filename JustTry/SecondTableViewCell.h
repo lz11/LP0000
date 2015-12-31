a00ed7a64dae8276e5aae413bcf44477443f88c0
lz11 <livspeng11@sina.com>
2015-12-31 10:14:41 +0800

    change 1

---
+++ JustTry/SecondTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SecondTableViewCell.h
+//  JustTry
+//
+//  Created by MS on 15/12/31.
+//  Copyright (c) 2015年 LP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondTableViewCell : UITableViewCell
+
+@end
---
+++ JustTry/SecondTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SecondTableViewCell.m
+//  JustTry
+//
+//  Created by MS on 15/12/31.
+//  Copyright (c) 2015年 LP. All rights reserved.
+//
+
+#import "SecondTableViewCell.h"
+
+@implementation SecondTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
